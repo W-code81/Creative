2f24e40cc1ca974961d25c135bc5e40791bd786e
oluwadamilolaaderogba@gmail.com <oluwadamilolaaderogba@gmail.com>
2026-06-25 11:43:27 +0100

    feat: used nuxtlink instead of anchor

--- app/components/Navbar.vue
+++ app/components/Navbar.vue
@@ -2,7 +2,7 @@
     <nav class="h-12 bg-black/20 backdrop-blur-md fixed top-0 left-0 right-0 z-50 flex items-center justify-between px-6 md:px-12 py-5 md:p-10">
         <a href="/" class="text-white">Logo</a>
         <div class="flex items-center justify-center gap-8 text-white cursor-pointer">
-            <a v-for="item in NAV_LINKS" :href="item.href">{{ item.label }}</a>
+            <NuxtLink v-for="item in NAV_LINKS" :key="item.label" :to="item.href">{{ item.label }}</NuxtLink>
         </div>
         <a href="/contact" class="text-white border p-2 rounded-xl hover:bg-white hover:text-black transition-all duration-200 ">Get in touch</a>
     </nav>
@@ -10,11 +10,6 @@
 
 
 <script lang="ts" setup>
-
-const NAV_LINKS = [
-    { label: "Home", href: "/" },
-    { label: "About", href: "/about" },
-    { label: "Projects", href: "/projects" }
-];
+import { NAV_LINKS } from '~/constants/navLink';
 
 </script>
