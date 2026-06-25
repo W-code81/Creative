6987bcb6e263099a35d3793bfd4cfc0fa077f88c
oluwadamilolaaderogba@gmail.com <oluwadamilolaaderogba@gmail.com>
2026-06-25 11:59:34 +0100

    feat: added opacity hover

--- app/components/Cards.vue
+++ app/components/Cards.vue
@@ -2,8 +2,8 @@
     <section class="flex flex-col md:flex-row gap-6 items-center justify-center">
         <div v-for="(item, index) in imgArray"
             :key="index"
-            class="w-80 h-100 rounded-2xl overflow-hidden  aspect-[3/4] mt-8">
-            <img :src="item.src" :alt="item.alt" class="w-full h-full object-cover">
+            class="w-80 h-100 rounded-2xl overflow-hidden  aspect-[3/4] mt-8 hover:-translate-y-2 transition-all duration-200">
+            <img :src="item.src" :alt="item.alt" class="w-full h-full object-cover opacity-80 hover:opacity-100 transition-all duration-200">
         </div>
 
 
