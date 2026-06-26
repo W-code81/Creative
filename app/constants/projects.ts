57b20a6c7a586763a28a5931ea8a5cbf110cecfb
oluwadamilolaaderogba@gmail.com <oluwadamilolaaderogba@gmail.com>
2026-06-26 16:34:47 +0100

    feat: created seperate projects data file

---
+++ app/constants/projects.ts
@@ -0,0 +1,36 @@
+import Coat from '/image/Coat.png'
+import Guy from '/image/Guy.png'
+import Pef from '/image/Pef.png'
+
+const projects = [
+  {
+    slug: "aura-brand-system",
+    title: 'Aura Brand System',
+    category: 'Brand Identity',
+    year: '2024',
+    image: Coat,
+  },
+  {
+    slug: "sensa-packaging",
+    title: 'Sensa Packaging',
+    category: 'Packaging',
+    year: '2024',
+    image: Pef,
+  },
+  {
+    slug: "volta-campaign",
+    title: 'Volta Campaign',
+    category: 'Creative Direction',
+    year: '2023',
+    image: Guy,
+  },
+  {
+    slug: "melo-identity",
+    title: 'Melo Identity',
+    category: 'Brand Identity',
+    year: '2023',
+    image: Coat,
+  },
+]
+
+export default projects
